fd8d8ae5e8d7e8a724f6e9cb1862461a4d63093d
Nameless <truemedian@gmail.com>
2019-08-04 20:09:46 -0500

    version bump

--- package.lua
+++ package.lua
@@ -1,6 +1,6 @@
 return {
 	name = "truemedian/lfs",
-	version = "0.1.0",
+	version = "0.1.1",
 	description = "A libuv equivalent for the popular lfs library",
 	tags = { "lua", "lfs", "filesystem" },
 	license = "MIT",
